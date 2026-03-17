9ff1695164c0c6027426e07cc6d10b99474a9c7a
wuttipattubs-oss <wuttipat.tubs@gmail.com>
2026-03-17 11:15:45 +0700

    Create grade checker by list .py
    ใช่codeต่างต่างที่เรียนเพื่อสร้างgrade checkerเพื่อใช่ตรวจเกรด

---
+++ grade checker by list .py
@@ -0,0 +1,78 @@
+# point=[]
+# first_point=int(input("student1 Enter your score: "))
+# point.append(first_point)
+# second_point=int(input("student2 Enter your score: "))
+# point.append(second_point)
+# third_point=int(input("student3 Enter your score: "))
+# point.append(third_point)
+# if  point[0] >= 50: 
+#     print("student1 is pass!"
+# else:
+#     print("student1 is fail!")
+# if  point[1] >= 50:
+#     print("student2 is pass!")
+# else:
+#     print("student2 is fail!")
+# if  point[2] >= 50:
+#     print("student3 is pass!")
+# else:
+#     print("student3 is fail!")
+# point=[]
+# first_point=int(input("student1 Enter your score: "))
+# point.append(first_point)
+# second_point=int(input("student2 Enter your score: "))
+# point.append(second_point)
+# third_point=int(input("student3 Enter your score: "))
+# point.append(third_point)
+# if  point[0] >= 50: 
+#     print("student1 is pass!")
+# else:
+#     print("student1 is fail!")
+# if  point[1] >= 50:
+#     print("student2 is pass!")
+# else:
+#     print("student2 is fail!")
+# if  point[2] >= 50:
+#     print("student3 is pass!")
+# else:
+#     print("student3 is fail!")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# point=[]
+# first_point=int(input("student1 Enter your score: "))
+# point.append(first_point)
+# second_point=int(input("student2 Enter your score: "))
+# point.append(second_point)
+# third_point=int(input("student3 Enter your score: "))
+# point.append(third_point)
+# if  point[0] >= 50: 
+#     check_point_one="pass"
+# else:
+#     check_point_one="fail"
+# if  point[1] >= 50:
+#     check_point_two="pass"
+# else:
+#     check_point_two="fail"
+# if  point[2] >= 50:
+#     check_point_three="pass"
+# else:
+#     check_point_three="fail"
+# print("student1:",check_point_one)
+# print("student2:",check_point_two)
+# print("student3:",check_point_three)
+
+
