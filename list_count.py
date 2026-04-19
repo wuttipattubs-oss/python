aca57484d6a701b42fb09f6798388a10b525776d
wuttipattubs-oss <wuttipat.tubs@gmail.com>
2026-04-19 18:05:13 +0700

    Create list_count.py
    Used count to count something in list

---
+++ list_count.py
@@ -0,0 +1,13 @@
+
+
+# cat = ["Red","Black","Green","Blue","Black"]
+# cat.append("Black")
+# cat_count=cat.count("Black")
+# print(cat_count)
+
+# coin = ["5", "10", "1", "10", "20", "4", "20", "20"]
+# coin.insert(2,"20")
+# print(coin)
+# coin_count = coin.count("20")
+# print(coin_count)
+# print(type(coin[2]))
