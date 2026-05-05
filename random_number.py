63507dc2d7c3da0102fa4022d109a0ecb409c1be
wuttipattubs-oss <wuttipat.tubs@gmail.com>
2026-05-05 11:13:47 +0700

    Create random_number.py
    random number

---
+++ random_number.py
@@ -0,0 +1,13 @@
+# answer=input("what 5+3")
+# if answer == "8":
+#     print("correct very good!")
+# else:
+#     print("try again you are noob")
+# import random
+# number = random.randint(1, 10)
+# guess=int(input("guess the lucky number1-10"))
+# print(number)
+# if guess == number:
+#     print("you win")
+# else:
+#     print("wrong")
